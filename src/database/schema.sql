5f556b45d04ed7724be2e6104aafaced830a6a2a
Nicolas Bizotto <bizotto.nicolas@gmail.com>
2023-05-14 21:44:58 -0300

    chore

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -2,12 +2,10 @@ CREATE DATABASE mycontacts;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-
 CREATE TABLE IF NOT EXISTS categories (
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
   name VARCHAR NOT NULL
 );
-
 
 CREATE TABLE IF NOT EXISTS contacts (
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
@@ -17,5 +15,3 @@ CREATE TABLE IF NOT EXISTS contacts (
   category_id UUID,
   FOREIGN KEY(category_id) REFERENCES categories(id)
 );
-
-
